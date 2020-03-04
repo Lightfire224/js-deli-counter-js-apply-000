a869139dd15f76c9fa6584b5a0cba99efad4297e
Elias Taveras <eliastave12@gmail.com>
2020-03-04 21:53:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, name) {
   
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
   
-  return `Welcome, $(name) `
+  return `Welcome, $(name). You are number $(katzDeliLine.length) in line.`
   
 }
 
